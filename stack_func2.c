42b7b1e24edcb68fd0f0ae842702e33972cf4f2e
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2023-10-19 05:35:23 -0700

    second stack

---
+++ stack_func2.c
@@ -0,0 +1,104 @@
+#include "monty.h"
+
+/**
+ * nop - Do nothing
+ * @line_number: line number of of the opcode
+ * @stack: pointing to top node of the stack
+ *
+ * Return: Void
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
+
+/**
+ * swap_nodes - Swaps the top two elements
+ * @line_number: line number of of the opcode
+ * @stack: pointing to top node of the stack
+ *
+ * Return: Void
+ */
+void swap_nodes(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "swap");
+	tmp = (*stack)->next;
+	(*stack)->next = tmp->next;
+	if (tmp->next != NULL)
+		tmp->next->prev = *stack;
+	tmp->next = *stack;
+	(*stack)->prev = tmp;
+	tmp->prev = NULL;
+	*stack = tmp;
+}
+
+/**
+ * add_nodes - Adds the top two elements of the stack
+ * @line_number: line number of of the opcode
+ * @stack: pointing to top node of the stack
+ *
+ * Return: Void
+ */
+void add_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "add");
+
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n + (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+
+/**
+ * sub_nodes - Adds the top two elements of the stack
+ * @line_number: line number of of the opcode
+ * @stack: pointing to top node of the stack
+ *
+ * Return: Void
+ */
+void sub_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+
+		more_err(8, line_number, "sub");
+
+
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n - (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+
+/**
+ * div_nodes - Adds the top two elements of the stack.
+ * @line_number: line number of of the opcode
+ * @stack: pointing to top node of the stack.
+ *
+ * Return: Void
+ */
+void div_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "div");
+
+	if ((*stack)->n == 0)
+		more_err(9, line_number);
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n / (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
